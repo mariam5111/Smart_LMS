c96052a323aedb313c999b54e79a79794a3c9f2c
mariam <mariamshahathamada@gmail.com>
2026-09-13 02:49:10 +0300

    fixes

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000/api',
+        url: 'https://smart-lms-api.onrender.com/api',
       },
     ],
     components: {
