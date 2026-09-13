7b7daaa6127017b208a92d4bf1de901324d4807a
mariam <mariamshahathamada@gmail.com>
2026-09-13 03:06:24 +0300

    fixes

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const dns = require('dns');
-
-dns.setServers(['8.8.8.8', '8.8.4.4']);
 const connectDB = require('./config/db');
 const app = require('./app');
 
